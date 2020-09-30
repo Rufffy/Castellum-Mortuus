0ecf46eb415d217525c1e16b667ff10a08caab72
Lucas <ima_lucas@hotmail.com>
2020-09-30 17:19:47 +1000

    Chasing Target
    Enemy Agent makes use of the nave mesh to constantly move towards the target Transform.

---
+++ Assets/Scripts/Enemy/Chasing.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chasing : MonoBehaviour
+{
+    public Transform chaseTarget;
+
+    bool isChasing = false;
+
+    public float waitTime = 3f;
+
+    float waitTimer = 0f;
+
+    public float stopDistance = 1f;
+
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isChasing)
+        {
+            agent.destination = chaseTarget.position;
+
+            if (agent.remainingDistance < stopDistance)
+            {
+                // This agent has reached the target
+                agent.isStopped = true;
+                agent.velocity = Vector3.zero;
+                isChasing = false;
+            }
+            else
+            {
+                agent.isStopped = false;
+            }
+        }
+        else
+        {
+            waitTimer += Time.deltaTime;
+            if (waitTimer > waitTime)
+            {
+                waitTimer = 0f;
+                isChasing = true;
+            }
+        }
+
+    }
+}
